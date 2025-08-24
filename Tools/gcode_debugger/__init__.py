9ae1ed746470a59991df1ddbdaa9a313827b1f91
Dave Carlton <davec@mac.com>
2025-08-24 13:36:42 -0500

    Removed gcode-debugger

--- Tools/gcode_debugger/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-gcode_debugger package initializer.
-
-Required for Python to recognize 'Tools.gcode_debugger' as an importable
-package when running tests directly from the repository root.
-"""
